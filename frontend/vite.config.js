03444a3070943be1a3043aa6a25501793c9970f2
jfchilito7 <jfchilito7@gmail.com>
2025-11-12 19:00:28 -0500

    feat: agregar configuración de Docker para el backend y frontend, incluyendo dependencias y puertos

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,16 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(),
+  plugins: [
+    vue(),
     tailwindcss()
   ],
+  // AGREGA ESTO PARA DOCKER:
+  server: {
+    host: '0.0.0.0', // Permite que Docker acceda desde fuera del contenedor
+    port: 5173,      // Fija el puerto estándar
+    watch: {
+      usePolling: true // Vital para que Windows/Docker detecten tus cambios al guardar
+    }
+  }
 })
